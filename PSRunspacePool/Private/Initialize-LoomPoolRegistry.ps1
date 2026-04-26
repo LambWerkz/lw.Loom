4d6373d6d1a2419a6774cbb3fb8d4fbd382a5e8b
LambWerkz <LambWerks@proton.me>
2026-04-26 15:19:45 -0400

    modified:   PSRunspacePool/Private/Initialize-LoomPoolRegistry.ps1

--- PSRunspacePool/Private/Initialize-LoomPoolRegistry.ps1
+++ PSRunspacePool/Private/Initialize-LoomPoolRegistry.ps1
@@ -1,13 +1,13 @@
 # Creates module-scoped ConcurrentDictionary, this is where the runspace pools will be stored. This function is idempotent, so it can be called multiple times without issue.
 function Initialize-LoomPoolRegistry {
     if ($Script:LoomPoolRegistry) {
-        if ($Script:LoomPoolRegistry -isnot [System.Collections.Concurrent.ConcurrentDictionary[string,object]]) {
+        if ($Script:LoomPoolRegistry -isnot [System.Collections.Concurrent.ConcurrentDictionary[string, object]]) {
             throw 'LoomPoolRegistry exists but is not the expected type.'
         }
 
         return $Script:LoomPoolRegistry
     }
 
-    $Script:LoomPoolRegistry = [System.Collections.Concurrent.ConcurrentDictionary[string,object]]::new()
+    $Script:LoomPoolRegistry = [System.Collections.Concurrent.ConcurrentDictionary[string, object]]::new()
     return $Script:LoomPoolRegistry
 }
